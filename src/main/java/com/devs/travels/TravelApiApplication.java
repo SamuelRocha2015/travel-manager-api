a869b09e32353dd7c9607b9e50ca2a405b924154
Samuel Rocha <samuelrrocha2016@gmail.com>
2019-11-26 18:56:46 -0500

    bootstrap project - init configuration.

---
+++ src/main/java/com/devs/travels/TravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.devs.travels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devs/travels/TravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.travels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
