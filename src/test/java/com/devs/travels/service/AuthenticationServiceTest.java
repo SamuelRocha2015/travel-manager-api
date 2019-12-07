036c1f9f68efd678ebafa0d0985b76b4bed62b0c
Samuel Rocha <samuelrodriguessr@hotmail.com>
2019-12-06 20:13:04 -0500

    #13 - tests fix

--- src/test/java/com/devs/travels/service/AuthenticationServiceTest.java
+++ src/test/java/com/devs/travels/service/AuthenticationServiceTest.java
@@ -1,9 +1,9 @@
 package com.devs.travels.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
-
+import com.devs.travels.databuilder.builder.LoginBuilder;
+import com.devs.travels.domain.Login;
+import com.devs.travels.exception.NotFoundException;
+import com.devs.travels.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,10 +12,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 
-import com.devs.travels.databuilder.builder.LoginBuilder;
-import com.devs.travels.domain.Login;
-import com.devs.travels.exception.NotFoundException;
-import com.devs.travels.repository.UserRepository;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthenticationServiceTest {
@@ -35,11 +33,11 @@ public class AuthenticationServiceTest {
     }
 
 	
-//	@Test
-//    void shouldGetExceptionWhenLoginNoExists() {
-//		when(repository.findByEmailAndIsActiveTrue(login.getEmail())).thenThrow(NotFoundException.class);
-//
-//		assertThrows(NotFoundException.class, () -> service.getAuthentication(login),
-//    			 AuthenticationService.USER_NOT_FOUND );
-//	}
+	@Test
+    void shouldGetExceptionWhenLoginNoExists() {
+		when(repository.existsByEmailAndIsActiveTrue(login.getEmail())).thenReturn(Boolean.FALSE);
+
+		assertThrows(NotFoundException.class, () -> service.getAuthentication(login),
+    			 AuthenticationService.USER_NOT_FOUND );
+	}
 }
